5b0cdf6418e31d02c5e5d54538ab82224f9158a6
loweffort-alt <farfan_alexander@outlook.com>
2024-02-08 13:06:58 -0500

    First commit: Server initialized

---
+++ server.js
@@ -0,0 +1,40 @@
+// Importa Express
+import express from "express";
+import cors from "cors";
+
+// Crea una aplicación Express
+const app = express();
+
+// Define una ruta para tu proxy
+app.use(cors());
+app.use("/proxy", async (req, res) => {
+  try {
+    // Realiza la solicitud a la URL deseada
+    const response = await fetch(
+      "https://servidor.ncn.pe/sensor/py/sismoIGP.json",
+    );
+
+    // Verifica si la solicitud fue exitosa
+    if (response.ok) {
+      // Convierte la respuesta a formato JSON
+      const data = await response.json();
+
+      // Envía los datos de vuelta al cliente
+      res.json(data);
+    } else {
+      // Si la solicitud no fue exitosa, devuelve un error
+      res
+        .status(response.status)
+        .json({ error: "No se pudo obtener los datos" });
+    }
+  } catch (error) {
+    // Si hay un error en la solicitud, devuelve un error
+    console.error("Error al obtener los datos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+// Inicia el servidor en el puerto 3000
+app.listen(3001, () => {
+  console.log("Servidor proxy escuchando en el puerto 3001");
+});
